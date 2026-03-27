3f6900ca09f0a55ce0d5665565c7e2c4039541f6
Paulo Ricardo <pauloricardo.st1812@hotmail.com>
2026-03-27 14:03:24 -0300

    banco de dados criado

--- main.py
+++ main.py
@@ -39,3 +39,38 @@ class Aluno(Base):
     #Função para imprimir
     def __repr__(self):
         return f"ID: {self.id} - NOME: {self.nome}"
+    
+
+class Curso(Base):
+    __tablename__ = "cursos"
+
+    #Como cria uma coluna
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
+    
+
+
+
+
+#Tabela Intermediaria
+inscricoes = Table(
+    "inscricoes", #nome da tabela
+    Base.metadata,
+    Column("aluno_id", Integer,ForeignKey("alunos.id"), primary_key=True),
+    Column("curso_id", Integer,ForeignKey("cursos.id"), primary_key=True),
+)
+
+
+
+
+#Conxão com db
+engine = create_engine("sqlite:///gestap_escolar.db")
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
